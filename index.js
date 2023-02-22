0db8250c6415a169c249560b8f242d33f3517719
Abdur Rahman Apu <abdurrahmany418@gmail.com>
2023-02-22 07:31:05 +0600

    mongodb added

--- index.js
+++ index.js
@@ -19,3 +19,19 @@ app.get('/', async (req, res) => {
 app.listen(port, async () => {
     console.log("Server is running on port", port);
 })
+
+async function run() {
+
+
+
+    const { MongoClient, ServerApiVersion } = require('mongodb');
+    const uri = `mongodb+srv://${process.env.DB_USER}:${DB_PASSWORD}@cluster0.7kbtzra.mongodb.net/?retryWrites=true&w=majority`;
+    const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+
+}
+
+run().catch(error => {
+    console.log(error);
+})
